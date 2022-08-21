af0ea79b3bcf9ca16bc86f93f2a536ba0a2d69dd
LSY <2tmdduq19@gmail.com>
2022-08-21 22:24:44 +0900

    data hub edited

--- scripts/data_hub.py
+++ scripts/data_hub.py
@@ -8,6 +8,7 @@ from mavsdk             import System
 from std_msgs.msg       import Float32MultiArray
 from geometry_msgs.msg  import Point32
 
+
 class DataHub:
     def __init__(self,  delt, traj_update_period,\
                         voxel_size, threshold, max_range, expension_size,\
@@ -51,11 +52,9 @@ class DataHub:
         ''' global position of home :
             
             lat (deg) : Lattitude
-
             lon (deg) : Longitude
            
             absolute_altitude_m : Altitude AMSL (above mean sea level) in metres
-
             relative_altitude_m :  Altitude relative to takeoff altitude in metres
           '''
 
@@ -63,29 +62,37 @@ class DataHub:
         self.posvel_ned = np.zeros(6)
 
         ''' local position about the home position :
-
             position.north_m 
-
             position.east_m
             
             position.down_m
-
             velocity.down_m
-
             velocity.down_m
-
             velocity.down_m
         '''
+
+
+        self.pos_global = np.zeros(4)
+
+        ''' local position about the home position :
+            Position type in global coordinates.
+            latitude_deg : double
+                Latitude in degrees (range: -90 to +90)
+            longitude_deg : double
+                Longitude in degrees (range: -180 to +180)
+            absolute_altitude_m : float
+                Altitude AMSL (above mean sea level) in metres
+            relative_altitude_m : float
+                Altitude relative to takeoff altitude in metres
+        '''
+
 
 
         self.attitude_eular = np.zeros(3)
 
         ''' Eular angle attitude :
-
             roll_deg : roll angle
-
             pitch_deg : pitch angle        
-
             yaw_deg : yaw angle                
         '''
 
@@ -93,9 +100,7 @@ class DataHub:
         self.attitude_quat = np.zeros(4)
 
         ''' Eular angle attitude :
-
             w, x, y, z 
-
         '''
 
 
@@ -103,9 +108,7 @@ class DataHub:
         self.is_connected = False
 
         '''
-
             Check connection with PX4
-
         '''
 
 
@@ -114,9 +117,7 @@ class DataHub:
         ''' Waypoints :
         
             Input for Trajectory Tracking mission
-
             for wp = [ n, e, d ]^T
-
             waypoints =  [ wp_0, wp_1, wp_2, ... , wp_n ]
         
         '''
@@ -125,7 +126,6 @@ class DataHub:
         self.img_bottom = None
 
         '''
-
             Compressed image of bottom camera
         
         '''
@@ -136,7 +136,6 @@ class DataHub:
         ''' Marker Position :
         
             position of the marker relative to drone
-
             marker_position = [ n, e, d ]^T 
         
         '''
@@ -145,7 +144,6 @@ class DataHub:
         self.state = "Disarm"
 
         ''' Current State
-
             1. Disarm
             2. Arm 
             3. Takeoff
@@ -154,7 +152,6 @@ class DataHub:
             6. PrepareLand
             7. Land
             10. EmergencyStop
-
             initial state is Disarm
         '''
 
@@ -162,13 +159,11 @@ class DataHub:
         self.action = None
 
         ''' Current Action requested
-
             1.disarm
             2.arm
             3.hold
             4.tracking
             5.land
-
             Action is None unless requested       
         '''
 
@@ -176,9 +171,7 @@ class DataHub:
         self.mission_input = None 
 
         ''' Mission Input
-
             0. e ( Emergency Stop : Failsafe mode landing )
-
             1. Arm
             2. Disarm
             3. Takeoff : target_altitude
@@ -205,7 +198,6 @@ class DataHub:
         self.armed = False
 
         ''' Is Armed
-
             armed    : True
             disarmed : False
         
@@ -215,7 +207,6 @@ class DataHub:
         self.is_in_air = False
 
         ''' Is in air
-
             in air    : True
             on ground : False
         
@@ -228,4 +219,3 @@ class DataHub:
         self.wp_viz = np.array([])
 
         self.jps_map = np.array([])
-        

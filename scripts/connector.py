fbf19dc8ca85d902fe13c15e6ddd66b7af9018e4
jeong-ho12 <82933963+jeong-ho12@users.noreply.github.com>
2022-08-24 20:49:01 +0900

    Update connector.py

--- scripts/connector.py
+++ scripts/connector.py
@@ -24,7 +24,8 @@ class Connector:
         
         '''
 
-        await self.drone.connect(system_address="udp://:14540")
+#         await self.drone.connect(system_address="udp://:14540")
+        await self.drone.connect(system_address="serial:///dev/ttyUSB0:921600")
 
         async for state in self.drone.core.connection_state():
             if state.is_connected:
